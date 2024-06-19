74c3ea8b9144750a8e374efaabc219c1cc1db00b
Torendra Rasik <torendrarasik@gmail.com>
2024-06-19 13:22:52 -0400

    (server): Adds initial spring app through spring initialzr

---
+++ spring-server/supportapi/src/main/java/com/trasik/supportapi/SupportapiApplication.java
@@ -0,0 +1,13 @@
+package com.trasik.supportapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportapiApplication.class, args);
+	}
+
+}
---
+++ spring-server/supportapi/src/test/java/com/trasik/supportapi/SupportapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trasik.supportapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

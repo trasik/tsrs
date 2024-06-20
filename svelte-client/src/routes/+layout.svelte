49919ee09613cd61b1bc31be8c9539e5bb6a37a1
Torendra Rasik <torendrarasik@gmail.com>
2024-06-20 10:32:19 -0400

    (client): Adds tailwindcss for fast prototyping

--- svelte-client/src/routes/+layout.svelte
+++ svelte-client/src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-    import Navigation from "$lib/components/Navigation.svelte";
+	import Navigation from '$lib/components/Navigation.svelte';
+	import '../app.css';
 </script>
 
 <Navigation />
---
+++ svelte-client/tailwind.config.ts
@@ -0,0 +1,81 @@
+import { fontFamily } from 'tailwindcss/defaultTheme';
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+	darkMode: ['class'],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	safelist: ['dark'],
+	theme: {
+		container: {
+			center: true,
+			padding: '2rem',
+			screens: {
+				'2xl': '1400px'
+			}
+		},
+		screens: {
+			'2xl': { max: '1535px' },
+			// => @media (max-width: 1535px) { ... }
+
+			xl: { max: '1279px' },
+			// => @media (max-width: 1279px) { ... }
+
+			lg: { max: '1023px' },
+			// => @media (max-width: 1023px) { ... }
+
+			md: { max: '767px' },
+			// => @media (max-width: 767px) { ... }
+
+			sm: { max: '639px' }
+			// => @media (max-width: 639px) { ... }
+		},
+		extend: {
+			colors: {
+				border: 'hsl(var(--border) / <alpha-value>)',
+				input: 'hsl(var(--input) / <alpha-value>)',
+				ring: 'hsl(var(--ring) / <alpha-value>)',
+				background: 'hsl(var(--background) / <alpha-value>)',
+				foreground: 'hsl(var(--foreground) / <alpha-value>)',
+				primary: {
+					DEFAULT: 'hsl(var(--primary) / <alpha-value>)',
+					foreground: 'hsl(var(--primary-foreground) / <alpha-value>)'
+				},
+				secondary: {
+					DEFAULT: 'hsl(var(--secondary) / <alpha-value>)',
+					foreground: 'hsl(var(--secondary-foreground) / <alpha-value>)'
+				},
+				destructive: {
+					DEFAULT: 'hsl(var(--destructive) / <alpha-value>)',
+					foreground: 'hsl(var(--destructive-foreground) / <alpha-value>)'
+				},
+				muted: {
+					DEFAULT: 'hsl(var(--muted) / <alpha-value>)',
+					foreground: 'hsl(var(--muted-foreground) / <alpha-value>)'
+				},
+				accent: {
+					DEFAULT: 'hsl(var(--accent) / <alpha-value>)',
+					foreground: 'hsl(var(--accent-foreground) / <alpha-value>)'
+				},
+				popover: {
+					DEFAULT: 'hsl(var(--popover) / <alpha-value>)',
+					foreground: 'hsl(var(--popover-foreground) / <alpha-value>)'
+				},
+				card: {
+					DEFAULT: 'hsl(var(--card) / <alpha-value>)',
+					foreground: 'hsl(var(--card-foreground) / <alpha-value>)'
+				}
+			},
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)'
+			},
+			fontFamily: {
+				sans: ['"Poppins"', ...fontFamily.sans],
+				bangers: ['"Bangers"', ...fontFamily.sans]
+			}
+		}
+	}
+};
+
+export default config;
